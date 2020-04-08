91a75b63fcc1ca70391fe92436f5080308c0d6fe
sungunjo <vfjdg12@gmail.com>
2020-04-08 23:28:38 +0900

    백준 2446번 문제 풀이 추가

---
+++ sungunjo/BJ_2446.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			printf(" ");
+		}
+		for (int j = ((2 * N) - (2 * i) - 1); j > 0; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i < N; i++) {
+		for (int j = 1; j < (N - i); j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < ((2 * i) + 1); j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
