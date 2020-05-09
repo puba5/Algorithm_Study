7289bf9b7fb40fbb158e5ee59561c2a5b6807eb0
sungunjo <vfjdg12@gmail.com>
2020-05-09 23:23:52 +0900

    백준 1992번 쿼드트리

---
+++ sungunjo/BJ_1992.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+	
+
+	return 0;
+}
