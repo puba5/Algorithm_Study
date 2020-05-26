f7a29e9c7e2418579c6c8643a4b50843b832f873
sungunjo <vfjdg12@gmail.com>
2020-05-26 23:47:01 +0900

    백준 2740번 행렬 곱셈 품

---
+++ sungunjo/BJ_2740.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int N, M, K;
+	scanf("%d %d", &N, &M);
+	vector<vector<int> > A(N, vector<int> (M));
+	for (int n = 0; n < N; n++) {
+		for (int m = 0; m < M; m++) {
+			scanf("%d", &(A[n][m]));
+		}
+	}
+
+	scanf("%d %d", &M, &K);
+	vector<vector<int> > B(M, vector<int> (K));
+	for (int m = 0; m < M; m++) {
+		for (int k = 0; k < K; k++) {
+			scanf("%d", &(B[m][k]));
+		}
+	}
+
+	vector<vector<int> > AB(N, vector<int> (K, 0));
+
+	for (int n = 0; n < N; n++) {
+		for (int k = 0; k < K; k++) {
+			for (int m = 0; m < M; m++) {
+				AB[n][k] += (A[n][m] * B[m][k]);
+				
+			}
+			cout << AB[n][k] << " ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
