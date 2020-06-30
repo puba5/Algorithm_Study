0530c6ae81e10aee44591894d30e8e5e761d754a
WooYoo <puba5@naver.com>
2020-07-01 01:53:48 +0900

    프로그래머스 영어 끝말잇기

---
+++ wooyoo/Programmers/코딩테스트연습/PR_영어_끝말잇기.js
@@ -0,0 +1,18 @@
+function solution(n, words) {
+  var answer = [0, 0];
+
+  let wordSet = new Set([words[0]]);
+
+  for (let i = 1; i < words.length; i++) {
+    if (wordSet.has(words[i])) {
+      return [(i % n) + 1, parseInt(i / n) + 1];
+    } else {
+      wordSet.add(words[i]);
+    }
+    if (words[i][0] != words[i - 1][words[i - 1].length - 1]) {
+      return [(i % n) + 1, parseInt(i / n) + 1];
+    }
+  }
+
+  return answer;
+}
