64136c047e792bd31db136944900dbe6b2290642
sungunjo <vfjdg12@gmail.com>
2020-06-30 23:43:51 +0900

    프로그래머스 자릿수 더하기 품

---
+++ sungunjo/PR_자릿수_더하기.js
@@ -0,0 +1,13 @@
+function solution(n)
+{
+    var answer = 0;
+    
+    while (n > 0) {
+        answer += (n % 10);
+        n = Math.floor(n / 10);
+    }
+
+    console.log('Hello Javascript')
+
+    return answer;
+}
