be3791ef4da6cb2c8ff16a62b575fea6e3ea6cd4
WooYoo <puba5@naver.com>
2020-07-10 00:11:35 +0900

    프로그래머스 키패드 누르기

--- wooyoo/Programmers/2020_카카오_인턴십/PR_키패드_누르기.js
+++ wooyoo/Programmers/2020_카카오_인턴십/PR_키패드_누르기.js
@@ -1,5 +1,49 @@
 function solution(numbers, hand) {
   var answer = "";
-
+  let rightX = 3;
+  let rightY = 2;
+  let leftX = 3;
+  let leftY = 0;
+  for (let number of numbers) {
+    let x, y;
+    if (number == 0) {
+      x = 3;
+      y = 1;
+    } else {
+      x = parseInt((number - 1) / 3);
+      y = (number - 1) % 3;
+    }
+    let gapRight = Math.abs(rightX - x) + Math.abs(rightY - y);
+    let gapLeft = Math.abs(leftX - x) + Math.abs(leftY - y);
+    if (y == 0) {
+      answer += "L";
+      leftX = x;
+      leftY = y;
+    } else if (y == 2) {
+      answer += "R";
+      rightX = x;
+      rightY = y;
+    } else {
+      if (gapRight == gapLeft) {
+        if (hand == "left") {
+          answer += "L";
+          leftX = x;
+          leftY = y;
+        } else if (hand === "right") {
+          answer += "R";
+          rightX = x;
+          rightY = y;
+        }
+      } else if (gapRight > gapLeft) {
+        answer += "L";
+        leftX = x;
+        leftY = y;
+      } else if (gapLeft > gapRight) {
+        answer += "R";
+        rightX = x;
+        rightY = y;
+      }
+    }
+  }
   return answer;
 }
